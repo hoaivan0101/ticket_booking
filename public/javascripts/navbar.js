ffdda93aea3f93b2f15e549b5cd8f0dbee6377b0
hoaivan0101 <hoaivan0101@yahoo.com>
2020-10-19 16:12:24 +0700

    fixnavbar

--- public/javascripts/navbar.js
+++ public/javascripts/navbar.js
@@ -2,9 +2,9 @@ $(document).ready(function () {
   $(window).scroll(function () {
     var pos_body = $("html, body").scrollTop();
     if (pos_body > 20) {
-      $(".header").addClass("stick_header");
+      $(".navbar_stick").addClass("stick_header");
     } else {
-      $(".header").removeClass("stick_header");
+      $(".navbar_stick").removeClass("stick_header");
     }
     if (pos_body < 500) {
       $(".backtotop").removeClass("unhide");
